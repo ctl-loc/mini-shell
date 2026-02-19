e0fd950aedb152561807f932ebb53df71e49b5cc
Simon Petrequin <simon.petrequin@free.fr>
2026-02-19 15:31:01 +0100

    feat: Allow bash command execution through msh

--- src/process.c
+++ src/process.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "process.h"
 #include "io.h"
@@ -48,11 +51,43 @@ char **split_line(char *line, int *argc)
     return tokens;
 }
 
+int launch_proc(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // Child process
+        if (execvp(args[0], args) == -1) // execute command
+        {
+            perror("msh");
+        }
+
+        exit(EXIT_FAILURE);
+    }
+    else if (pid > 0)
+    {
+        // Parent process
+        do
+        {
+            // wait for the child to finish
+            wpid = waitpid(pid, &status, WUNTRACED);
+            printf("pid: %d\n", wpid);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status)); // child exited normally or was killed by a signal
+    }
+    else
+    {
+        // Error forking
+        perror("msh");
+    }
+
+    return 1;
+}
+
 int execute(int argc, char **args)
 {
-    for (int i = 0; i < argc; i++)
-    {
-        printf("%s\n", args[i]);
-    }
+    launch_proc(args);
     return 0;
 }
